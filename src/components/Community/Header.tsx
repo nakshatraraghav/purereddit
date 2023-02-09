c1f411dcaa35f49551991fa1c97c12f714a77d1e
nakshatra raghav <combatrickshaw@gmail.com>
2023-02-09 20:19:30 +0530

    (ui): updated community page button styles

--- src/components/Community/Header.tsx
+++ src/components/Community/Header.tsx
@@ -8,7 +8,7 @@ type HeaderProps = {
 };
 
 const Header: React.FC<HeaderProps> = ({ communityData }) => {
-  const isJoined: boolean = false; // we will add the communitySnippets to the state and the check if they
+  const isJoined: boolean = true; // we will add the communitySnippets to the state and the check if they
   // if this community is available in the state, if so then we will change this isJoined button to a state var
   // and do suitable changes
   return (
@@ -30,7 +30,7 @@ const Header: React.FC<HeaderProps> = ({ communityData }) => {
               style={{ borderRadius: "50%" }}
             />
           )}
-          <div className="flex p-5">
+          <div className="flex p-5 items-center">
             <div className="flex flex-col mr-6">
               <div className="font-semibold text-2xl">{communityData.id}</div>
               <div className="font-semibold text-sm text-gray-500">
@@ -38,11 +38,11 @@ const Header: React.FC<HeaderProps> = ({ communityData }) => {
               </div>
             </div>
             {isJoined ? (
-              <button className="bg-blue-500 px-6 hover:bg-blue-700 text-white font-bold rounded-2xl transition-all duration-300">
+              <button className="bg-white hover:bg-gray-100 h-[40px] text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-2xl shadow transition-all duration-300">
                 Leave
               </button>
             ) : (
-              <button className="bg-blue-500 px-6 hover:bg-blue-700 text-white font-bold rounded-2xl  transition-all duration-300">
+              <button className="bg-blue-500 px-6 h-[40px] hover:bg-blue-700 text-white font-bold rounded-2xl transition-all duration-300">
                 Join
               </button>
             )}
